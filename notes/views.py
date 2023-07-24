1c06bb04560db73dbe6b5c0d3ccac359fbb03f2e
Kay Lizadhi <khairinka.rania01@ui.ac.id>
2023-07-24 20:59:12 +0700

    added post note and delete flutter in views.py

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,17 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, QueryDict
 from django.core import serializers
 from django.shortcuts import (get_object_or_404, render, HttpResponseRedirect)
 from .models import MyNotes
 from .forms import NotesForm
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def show_json(request):
+    data = MyNotes.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 def show_json_by_id(request, id):
     data = MyNotes.objects.filter(id=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
@@ -44,6 +50,30 @@ def create(request):
 
     context['form'] = form
     return render(request, "create.html", context)
+
+@csrf_exempt
+def post_note(request):
+    if (request.method == "POST"):
+        request_data = request.body
+        request_data = json.loads(request_data.decode('utf-8'))
+        data = QueryDict('', mutable = True)
+        data.update(request_data)
+        form_req = NotesForm(data)
+
+        if form_req.is_valid():
+            form_req.save()
+
+        return HttpResponseRedirect("/notes")
+
+@csrf_exempt
+def delete_flutter(request):
+    if (request.method == "POST"):
+        request_data = request.body
+        pk = json.loads(request_data.decode('utf-8'))['pk']
+        note = MyNotes.objects.get(id=pk)
+        note.delete()
+    
+    return HttpResponseRedirect("/notes")
 
 def read(request, id):
     context = {}
